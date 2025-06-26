f756e9dbdec6de65b3d8f6b4e030fcd7a09a2cfe
Jupower38300 <julien.legrand@my-digital-school.org>
2025-06-26 11:13:55 +0200

    Fixes in  backend

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://blazed-app.vercel.app',
     credentials: true,
   });
 
@@ -23,6 +23,6 @@ async function bootstrap() {
 
   app.useGlobalGuards(new JwtGuard(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 10000);
 }
 bootstrap();
